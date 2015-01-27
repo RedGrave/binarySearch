05893f0e068ac5c25af665b712856ab6bd19f2fb
Thomas Jolain <thomas.jolain@gmail.com>
2015-01-27 21:04:38 +0100

    First real commit

---
+++ binarySearch/Node.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace binarySearch
+{
+    public class Node
+    {
+        public int data { get; set;}
+        public Node leftNode { get; set;}
+        public Node rightNode { get; set; }
+
+        public Node(int new_data)
+        {
+            this.data = new_data;
+            this.leftNode = null;
+            this.rightNode = null;
+        }
+
+        public int searchNode(int value, int actualLoop)
+        {
+            int nbrOfStep = 0;
+            Console.Write(this.data + "-->");
+
+            if (value < this.data)
+            {
+                if (this.rightNode != null)
+                {
+                    if (this.rightNode.data == value)
+                    {
+                        nbrOfStep = actualLoop + 1;
+                    }
+
+                    else
+                    {
+                        nbrOfStep = rightNode.searchNode(value, actualLoop + 1);
+                    }
+                }
+            }
+
+            if(value > this.data)
+            {
+                if (this.leftNode != null)
+                {
+                    if (this.leftNode.data == value)
+                    {
+                        nbrOfStep = actualLoop + 1;
+                    }
+
+                    else
+                    {
+                        nbrOfStep = leftNode.searchNode(value, actualLoop + 1);
+                    }
+                }
+            }
+
+            /*
+
+            if(this.leftNode.data == value || this.rightNode.data == value)
+            {
+                nbrOfStep = actualLoop + 1;
+            }
+            
+            else
+            {
+                if(value > this.data)
+                {
+                    nbrOfStep = leftNode.searchNode(value, actualLoop + 1);
+                }
+                if (value < this.data)
+                {
+                    nbrOfStep = rightNode.searchNode(value, actualLoop + 1);
+                }
+            }*/
+            return nbrOfStep;
+        }
+
+        public void insertNode(Node newNode)
+        {
+            if(newNode.data > this.data)
+            {
+                if(leftNode != null)
+                {
+                    this.leftNode.insertNode(newNode);
+                }
+                else
+                {
+                    this.leftNode = newNode;
+                }
+            }
+
+            if(newNode.data < this.data)
+            {
+                if (rightNode != null)
+                {
+                    this.rightNode.insertNode(newNode);
+                }
+                else
+                {
+                    this.rightNode = newNode;
+                }
+            }
+        }
+    }
+
+    public class RootNode
+    {
+        public RootNode()
+        {
+
+        }
+        public Node populateTree(int[] numbers)
+        {
+            Node n = new Node(numbers[0]);
+
+            for (int i = 1; i < numbers.Count(); i++)
+            {
+                Node x = new Node(numbers[i]);
+                n.insertNode(x);
+            }
+            return n;
+        }
+    }
+}
---
+++ binarySearch/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binarySearch
+{
+    class Program
+    {
+        static int[] regenerateTree(int nbrOfNbr, int max)
+        {
+            Random rand = new Random();
+            int[] data = new int[nbrOfNbr];
+            for(int i = 0; i < nbrOfNbr; i++)
+            {
+                data[i] = rand.Next() % max;
+            }
+            return data;
+        }
+
+        static void askQuestion(Node n)
+        {
+            Console.WriteLine("READY!");
+            while(true)
+            {
+                string s = Console.ReadLine().ToString();
+                if (s == "exit") break;
+
+                int srch;
+                if(int.TryParse(s, out srch))
+                {
+                    int result = n.searchNode(srch, 0);
+                    if(result != 0)
+                    {
+                        Console.WriteLine("FOUND ! STEP : " + result);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("NOT FOUND !");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Failed to parse number");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] data = regenerateTree(1000000, 10000);
+            RootNode rn = new RootNode();
+            Node n = rn.populateTree(data);
+
+            askQuestion(n);
+            
+        }
+    }
+}
